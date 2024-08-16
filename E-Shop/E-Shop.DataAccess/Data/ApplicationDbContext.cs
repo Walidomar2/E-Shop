f777f4197f09ff984008d6b01f3a7339fe7b492b
Walid Omar <walidomar291@gmail.com>
2024-08-16 05:24:28 +0300

    Add project files.

---
+++ E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Shop.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Shop.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+            
+        
+
+    }
+}
---
+++ E-Shop/E-Shop.Domain/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Shop.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+    }
+}
