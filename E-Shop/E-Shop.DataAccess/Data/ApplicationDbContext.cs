cc2814a7a513e0b822e3eabcae5497ceb0b7a6ab
Walid Omar <walidomar291@gmail.com>
2024-08-31 19:14:38 +0300

    adding extra properties for user

--- E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
+++ E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace E_Shop.DataAccess.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         
     }
