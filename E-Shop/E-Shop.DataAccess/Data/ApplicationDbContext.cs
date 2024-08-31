7c2cab55fdc513cb4317619ffc2ae493b477b5ba
Walid Omar <walidomar291@gmail.com>
2024-08-31 23:06:03 +0300

    fixing adding extra properties bugs

--- E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
+++ E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using E_Shop.Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_Shop.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         
     }
---
+++ E-Shop/E-Shop.Utilities/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Shop.Utilities
+{
+    public static class SD
+    {
+        public static string AdminRole = "Admin";
+        public static string EditRole = "Editor";
+        public static string CustomerRole = "Customer";
+
+    }
+}
