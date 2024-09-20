b13ac38602f7bdd7ec615426a4e7850c7eae3b40
Walid Omar <walidomar291@gmail.com>
2024-09-20 23:55:54 +0300

    showing registered users by admin

---
+++ E-Shop/E-Shop.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace E_Shop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            string userId = claim.Value;
+
+            return View(_context.ApplicationUsers.Where(x=> x.Id != userId).ToList());
+        }
+    }
+}
--- E-Shop/E-Shop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ E-Shop/E-Shop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -140,7 +140,7 @@ namespace E_Shop.Web.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    await _userManager.AddToRoleAsync(user, SD.Customer);
+                    await _userManager.AddToRoleAsync(user, SD.Admin);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
