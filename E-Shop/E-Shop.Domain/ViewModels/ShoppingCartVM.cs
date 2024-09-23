1035fe793684db9dcb5ea1996edd4002f412e430
Walid Omar <walidomar291@gmail.com>
2024-09-23 15:04:32 +0300

    shopping cart UI and Total Price

---
+++ E-Shop/E-Shop.Domain/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,11 @@
+﻿using E_Shop.Domain.Models;
+
+
+namespace E_Shop.Domain.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> CartList { get; set; }
+        public decimal TotalCarts { get; set; }
+    }
+}
---
+++ E-Shop/E-Shop.Web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,41 @@
+﻿using E_Shop.Domain.Interfaces;
+using E_Shop.Domain.Models;
+using E_Shop.Domain.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace E_Shop.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["HeaderTitle"] = "Your Cart";
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                CartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, experession: "Product")
+            };
+
+            foreach (var cart in ShoppingCartVM.CartList)
+            { 
+                ShoppingCartVM.TotalCarts = (cart.Count * cart.Product.Price);
+            }
+            return View(ShoppingCartVM);
+        }
+    }
+}
