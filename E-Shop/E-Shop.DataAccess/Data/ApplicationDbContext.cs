05868d8169e2cf9909d4630f9e3d916adf7b8531
Walid Omar <walidomar291@gmail.com>
2024-09-24 15:20:45 +0300

    adding OrderHeader and OrderDetails Models

--- E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
+++ E-Shop/E-Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ namespace E_Shop.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         
     }
--- E-Shop/E-Shop.DataAccess/Migrations/20240924121902_AddingOrderHeaderAndOrderDetailsTables.Designer.cs
+++ E-Shop/E-Shop.DataAccess/Migrations/20240924121902_AddingOrderHeaderAndOrderDetailsTables.Designer.cs
@@ -4,6 +4,7 @@ using E_Shop.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_Shop.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240924121902_AddingOrderHeaderAndOrderDetailsTables")]
+    partial class AddingOrderHeaderAndOrderDetailsTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
