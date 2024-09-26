adbcb3b7ae4e24713038b1ebfa799c1befc3a6a8
Walid Omar <walidomar291@gmail.com>
2024-09-26 16:10:15 +0300

    fixing admin dashboard design

---
+++ E-Shop/E-Shop.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using E_Shop.Domain.Interfaces;
+using E_Shop.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Admin)]
+    public class DashboardController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Orders = _unitOfWork.OrderHeader.GetAll().Count();
+            ViewBag.ApprovedOrders = _unitOfWork.OrderHeader.GetAll(x => x.OrderStatus == SD.Approve).Count();
+            ViewBag.Users = _unitOfWork.ApplicationUser.GetAll().Count();
+            ViewBag.Products = _unitOfWork.Product.GetAll().Count();
+            return View();
+        }
+    }
+}
